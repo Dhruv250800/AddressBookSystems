b4f8fc24b76282e17de358dfb34512515780942f
DhruvGour <dhruv250800@gmail.com>
2023-06-20 19:16:10 +0530

    [DhruvGour]-UC1

---
+++ AddressBook.cs
@@ -0,0 +1,27 @@
+﻿internal class AddressBook
+{
+    List<Contact> con = new List<Contact>();
+    public void AddContact()
+    {
+        Contact contact = new Contact();
+
+        Console.WriteLine("Enter FirstName : ");
+        contact.Firstname = Console.ReadLine();
+        Console.WriteLine("Enter LastName : ");
+        contact.Lastname = Console.ReadLine();
+        Console.WriteLine("Enter Address : ");
+        contact.Address = Console.ReadLine();
+        Console.WriteLine("Enter City : ");
+        contact.City = Console.ReadLine();
+        Console.WriteLine("Enter State : ");
+        contact.State = Console.ReadLine();
+        Console.WriteLine("Enter Zip : ");
+        contact.Zip = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter PhoneNumber : ");
+        contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine("Enter E-mail : ");
+        contact.Email = Console.ReadLine();
+
+        con.Add(contact);
+    }
+}
---
+++ Contact.cs
@@ -0,0 +1,11 @@
+﻿internal class Contact
+{
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public long Zip { get; set; }
+    public long PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
